9c2106ad37cdd0023502966b439f07db85d32fbc
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2025-09-13 22:41:21 -0400

    Adding some basic parsers

---
+++ libUniversalRandomizer/src/main/java/redactedrice/universalrandomizer/parser/GetterParser.java
@@ -0,0 +1,57 @@
+package redactedrice.universalrandomizer.parser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import redactedrice.modularparser.core.Response;
+import redactedrice.modularparser.lineformer.Grouper;
+import redactedrice.modularparser.literal.BaseArgumentLiteral;
+import redactedrice.modularparser.utils.ArgumentUtils;
+import redactedrice.reflectionhelpers.utils.ReflectionUtils;
+import redactedrice.universalrandomizer.userobjectapis.Getter;
+
+public class GetterParser extends BaseArgumentLiteral {
+    protected static final String[] argsOrdered = new String[] {"path"};
+
+	public GetterParser() {
+		this(null);
+	}
+	
+	public GetterParser(Grouper grouper) {
+		super(GetterParser.class.getSimpleName(), "Getter", grouper,
+		        new String[] {argsOrdered[0]},
+		        new String[] {},
+		        new Object[] {});
+	}
+	
+	@Override
+	public Response<Object> tryEvaluateObject(Map<String, Object> args) {
+		Response<String> path = ArgumentUtils.argToType(argsOrdered[0], args, String.class);
+		if (path.wasError()) {
+			return path.convert(Object.class);
+		}
+		return parsePath(path.getValue()).convert(Object.class);
+	}
+	
+	protected static Response<Getter<Object, Object>> parsePath(String path) {
+		Getter<Object, Object> getter = o -> {
+			try {
+				return ReflectionUtils.getVariable(o, path);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+					| NoSuchMethodException | SecurityException | NoSuchFieldException e) {
+				return null;
+			}
+		};
+	    return Response.is(getter);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Response<Getter<Object, Object>> parse(Object getter) {
+		if (getter instanceof Getter<?,?> asGetter) {
+			return Response.is((Getter<Object, Object>) asGetter);
+		} else if (getter instanceof String asString) {
+			return parsePath(asString);
+		}
+		return Response.error("Failed to parse Getter object or path string");
+	}
+}
---
+++ libUniversalRandomizer/src/main/java/redactedrice/universalrandomizer/parser/MultiGetterParser.java
@@ -0,0 +1,50 @@
+package redactedrice.universalrandomizer.parser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import redactedrice.modularparser.core.Response;
+import redactedrice.modularparser.lineformer.Grouper;
+import redactedrice.modularparser.literal.BaseArgumentLiteral;
+import redactedrice.modularparser.utils.ArgumentUtils;
+import redactedrice.reflectionhelpers.utils.ReflectionUtils;
+import redactedrice.universalrandomizer.userobjectapis.MultiGetter;
+
+public class MultiGetterParser extends BaseArgumentLiteral {
+    protected static final String[] argsOrdered = new String[] {"path"};
+
+	public MultiGetterParser() {
+		this(null);
+	}
+	
+	public MultiGetterParser(Grouper grouper) {
+		super(MultiGetterParser.class.getSimpleName(), "MultiGetter", grouper,
+		        new String[] {argsOrdered[0]},
+		        new String[] {},
+		        new Object[] {});
+	}
+	
+	@Override
+	public Response<Object> tryEvaluateObject(Map<String, Object> args) {
+		try {
+			Response<String> path = ArgumentUtils.argToType(argsOrdered[0], args, String.class);
+			if (path.wasError()) {
+				return path.convert(Object.class);
+			}
+
+	        MultiGetter<?, ?> multiGetter = (o, cnt) -> {
+				try {
+					return ReflectionUtils.invoke(o, path.getValue(), cnt);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+						| NoSuchMethodException | SecurityException | NoSuchFieldException e) {
+					// TODO Auto-generated catch block
+					return null;
+				}
+			};
+		    return Response.is(multiGetter);
+		} catch (ClassCastException e) {
+		    // This should have been handled if this was called
+		    return Response.error("failed to cast value: " + e.getMessage());
+		}
+	}
+}
